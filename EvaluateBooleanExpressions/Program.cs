9f11b381404b5d71a1666c599cc706dd7b14f34e
Camanrofo <camanrofo@gmail.com>
2023-10-17 08:18:28 -0500

    Add boolean expressions

---
+++ EvaluateBooleanExpressions/Program.cs
@@ -0,0 +1,14 @@
+﻿/*First Excercise
+Random random = new Random();
+int coin = random.Next(0,2);
+Console.WriteLine($" { (coin <= 1 ? "Tails" : "Head" )} ");
+*/
+
+string permission = "Admin|Manager";
+int level = 55;
+bool isAdmin = permission.Contains("Admin") ? true : false ;
+if (isAdmin && level>55)Console.WriteLine("Welcome, Super Admin user.");
+else if (isAdmin && level<=55)Console.WriteLine("Welcome, Admin user.");
+else if(!isAdmin && level>20)Console.WriteLine("Contact an Admin for access.");
+else if (!isAdmin&& level<=20)Console.WriteLine("You do not have sufficient privileges.");
+else Console.WriteLine("You do not have sufficient privileges.");
